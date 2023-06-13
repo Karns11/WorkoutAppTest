6e53f40c004a6d4ffe802941c883d0b38f85cf19
nathan karns <Nathan k>
2023-06-13 19:29:18 -0400

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,38 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useSelector } from "react-redux";
+import HomeScreen from "./screens/HomeScreen";
+import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
+import MainScreen from "./screens/MainScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+import WorkoutScreen from "./screens/WorkoutScreen";
+import AddFriendsScreen from "./screens/AddFriendsScreen";
+import FriendScreen from "./screens/FriendScreen";
+
+// const PrivateRoute = ({ element: Element, ...rest }) => {
+//   const { userInfo } = useSelector((state) => state.auth);
+
+//   return userInfo ? <Element /> : <Navigate to="/login" replace />;
+// };
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomeScreen />} />
+        <Route path="/login" element={<LoginScreen />} />
+        <Route path="/register" element={<RegisterScreen />} />
+        <Route path="/mainscreen" element={<MainScreen />} />
+        <Route path="/profile" element={<ProfileScreen />} />
+        <Route path="/users/workout/:id" element={<WorkoutScreen />} />
+        <Route path="/profile/addfriends" element={<AddFriendsScreen />} />
+        <Route path="/profile/friends/:friendId" element={<FriendScreen />} />
+      </Routes>
+      <ToastContainer />
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ frontend/src/screens/MainScreen2.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import MainHeader from "../components/MainHeader";
+
+const MainScreen2 = () => {
+  return <div>Main Screen2</div>;
+};
+
+export default MainScreen2;
